c2a9a9a111aa84910af2d65046b05d28f8270356
ANDRE <klai1@mail.ru>
2022-07-30 18:15:22 +0400

    Дз мое

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int age = 19;
+        boolean canNoGoTuSchool=age<7||age>18;
+        if(age>18&&age<21){
+            System.out.println("Ты достаточно взрослый, что бы водить, но недостаточно взрослый, что бы пить Алкоголь");}
+        if (canNoGoTuSchool)
+        {System.out.println("Я думаю ты не ходишь в школу");
+        }
+
+
+
+        }
+    }
