14d4576b84ca13d785c73f5ba5fd0fc3f72cfb1a
ANDRE <klai1@mail.ru>
2022-07-30 19:37:22 +0400

    Дз мое

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 19;
-        boolean canNoGoTuSchool=age<7||age>18;
-        if(age>18&&age<21){
-            System.out.println("Ты взрослый, что бы водить, но недостаточно взрослый, что бы пить Алкоголь");}
 
 
 
